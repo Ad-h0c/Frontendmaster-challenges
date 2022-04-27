d6e11b90c49a42435c3d8e3f56818c8aad2266da
Ad-h0c <reddycsivaprakash5@gmail.com>
2022-04-27 13:02:20 +0530

    Added new files

---
+++ 1_Beginner/interactive-rating-component-main/tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      mb: "375px",
+    },
+    extend: {
+      colors: {
+        Orange: "hsl(25, 97%, 53%)",
+        White: "hsl(0, 0%, 100%)",
+        LightGrey: "hsl(217, 12%, 63%)",
+        MediumGrey: "hsl(216, 12%, 54%)",
+        DarkBlue: "hsl(213, 19%, 18%)",
+        VeryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+      fontFamily: {
+        overpass: ["Overpass", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
